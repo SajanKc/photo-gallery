bcd58cfaccfee3d131d35656cdb46940d4d6a800
SajanKc <sajan1996kc@gmail.com>
2020-09-11 16:16:33 +0545

    connected with firebase database

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './css/Title.css';
+import '../css/Title.css';
 
 const Title = () => {
   return (
     <div className="title">
       <h1>Photo Gallery</h1>
-      <h2>Your Pictures</h2>
+      <h2>My Pictures</h2>
       <p>Watch my photo collection.</p>
     </div>
   )
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../css/UploadForm.css';
+import ProgressBar from './ProgressBar';
 
-export const UploadForm = () => {
+const UploadForm = () => {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpeg', 'image/svg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        console.log(selected);
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError("Please select an image file with extension (png, jpeg or svg)");
+        }
+    }
+
     return (
         <div className="upload">
             <form>
-                <input type="file" />
-                <h1> this is test</h1>
+                <label>
+                    <input type="file" onChange={changeHandler} />
+                    <span>+</span>
+                </label>
+                <div className="output">
+                    {error && <div className="error">{error}</div>}
+                    {file && <div className="success">{file.name}</div>}
+                    {file && <ProgressBar file={file} setFile={setFile}/>}
+                </div>
             </form>
         </div>
     )
 }
+
+export default UploadForm;
