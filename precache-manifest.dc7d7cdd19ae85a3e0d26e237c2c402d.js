81ac74cf55e49996e1482b30af1f357066eab56d
SajanKc <sajan1996kc@gmail.com>
2020-10-03 20:50:52 +0545

    Updates

---
+++ precache-manifest.dc7d7cdd19ae85a3e0d26e237c2c402d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f9868575c06ede513666121fe6a61f",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "8c2157a6241b845c7c53",
+    "url": "/photo-gallery/static/css/main.5c7202a4.chunk.css"
+  },
+  {
+    "revision": "3001ff00402c026f2764",
+    "url": "/photo-gallery/static/js/2.3087b4dc.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/photo-gallery/static/js/2.3087b4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2157a6241b845c7c53",
+    "url": "/photo-gallery/static/js/main.96c9ef67.chunk.js"
+  },
+  {
+    "revision": "dea700b16e354df5ea7c",
+    "url": "/photo-gallery/static/js/runtime-main.2fb2e379.js"
+  }
+]);
